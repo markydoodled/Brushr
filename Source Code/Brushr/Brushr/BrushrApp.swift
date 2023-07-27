3a675e9b298aed71ca1b4ae605cda4c1baf9a083
Mark Howard <markhoward2005@gmail.com>
2023-07-27 22:43:36 +0100

    Added Inital Project Code

---
+++ Source Code/Brushr/Brushr/BrushrApp.swift
@@ -0,0 +1,18 @@
+//
+//  BrushrApp.swift
+//  Brushr
+//
+//  Created by Mark Howard on 27/07/2023.
+//
+
+import SwiftUI
+import HealthKit
+
+@main
+struct BrushrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
