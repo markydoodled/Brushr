497ae92c3f1d0655ccf30817bf70a95e55349361
Mark Howard <markhoward2005@gmail.com>
2023-07-28 09:53:30 +0100

    Added watchOS Base Project

---
+++ Source Code/Brushr/Brushr watchOS Watch App/Brushr_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brushr_watchOSApp.swift
+//  Brushr watchOS Watch App
+//
+//  Created by Mark Howard on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Brushr_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Source Code/Brushr/Brushr watchOS Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Brushr watchOS Watch App
+//
+//  Created by Mark Howard on 28/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
