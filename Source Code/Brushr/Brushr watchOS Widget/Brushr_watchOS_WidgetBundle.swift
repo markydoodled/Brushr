505234eaecc595f7e6e42b6a1221bbcc3de39536
Mark Howard <markhoward2005@gmail.com>
2023-08-01 20:06:54 +0100

    Added Base watchOS Widget Extension Code And UI

---
+++ Source Code/Brushr/Brushr watchOS Widget/Brushr_watchOS_WidgetBundle.swift
@@ -0,0 +1,22 @@
+//
+//  Brushr_watchOS_WidgetBundle.swift
+//  Brushr watchOS WidgetExtension
+//
+//  Created by Mark Howard on 01/08/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Brushr_watchOS_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Brushr_watchOS_Widget()
+        Brushr_30_watchOS_Widget()
+        Brushr_1_watchOS_Widget()
+        Brushr_2_watchOS_Widget()
+        Brushr_3_watchOS_Widget()
+        Brushr_4_watchOS_Widget()
+        Brushr_5_watchOS_Widget()
+    }
+}
